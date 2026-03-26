8b1750d8bf4823c57ea9b7c94f05dabc07224bfb
gusalberto <gustavoasouzasilva@gmail.com>
2026-03-26 12:46:35 -0300

    style: remove build output directory configuration from Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import laravel from 'laravel-vite-plugin';
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-    },
     plugins: [laravel({
         input: ['resources/css/app.css', 'resources/js/app.js'],
         refresh: true,
